ba6bbe62776f480a8b48c91f410fe161059a47fd
Xaver <xaver@everpress.co>
2024-05-27 15:10:28 +0200

    updated readme

--- mailster-kickbox-io.php
+++ mailster-kickbox-io.php
@@ -1,9 +1,10 @@
 <?php
 /*
 Plugin Name: Mailster Kickbox
+Requires Plugins: mailster
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=Kickbox
 Description: Verifies your subscribers email addresses with Kickbox
-Version: 1.2
+Version: 1.2.0
 Author: EverPress
 Author URI: https://mailster.co
 Text Domain: mailster-kickbox
@@ -11,10 +12,9 @@ License: GPLv2 or later
 */
 
 
-define( 'MAILSTER_KICKBOX_VERSION', '1.2' );
-define( 'MAILSTER_KICKBOX_REQUIRED_VERSION', '2.2' );
+define( 'MAILSTER_KICKBOX_VERSION', '1.2.0' );
+define( 'MAILSTER_KICKBOX_REQUIRED_VERSION', '4.0' );
 define( 'MAILSTER_KICKBOX_FILE', __FILE__ );
 
-require_once dirname( __FILE__ ) . '/classes/kickbox.class.php';
+require_once __DIR__ . '/classes/kickbox.class.php';
 new MailsterKickBox();
-
